3e67632c40be49e7fef305bf31e9b0d819f85123
Dan Nowacki <dnowacki@usgs.gov>
2019-05-22 15:17:48 -0700

    Run scripts

---
+++ run_all_pcmsc_nmb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+python pcmsc_nmb.py NMB14M1T01awWvs-p.nc
+python pcmsc_nmb.py NMB14SCW01rbrWvs-p.nc
+python pcmsc_nmb.py NMB15M1T03aqd.nc
+python pcmsc_nmb.py NMB15SCW01rbrWvs-p.nc
