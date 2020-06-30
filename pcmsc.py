4dba5e66e786c98cbad73790a0f399b7f5c5aff5
Dan Nowacki <dnowacki@usgs.gov>
2020-06-30 19:34:00 +0000

    changes for ingesting more PCMSC data

---
+++ pcmsc.py
@@ -0,0 +1,171 @@
+import xarray as xr
+import pandas as pd
+import numpy as np
+import sys
+import stglib
+import portal
+import os
+import netCDF4
+from pathlib import Path
+
+
+def add_title_history(ds, doi, title, summary):
+    if "title" not in ds.attrs:
+        ds.attrs["title"] = title
+    else:
+        ds.attrs["title"] = title + "; Original title: " + ds.attrs["title"]
+
+    if "history" in ds.attrs and (len(ds.attrs["history"]) > 0):
+        histtext = ds.attrs["history"]
+    else:
+        histtext = ""
+
+    ds.attrs["history"] = (
+        "Imported from ScienceBase data release and converted to CF using process_"
+        + doi
+        + ".ipynb; "
+        + histtext
+    )
+
+    if "summary" not in ds.attrs:
+        ds.attrs["summary"] = """"""
+    else:
+        ds.attrs["summary"] = summary + "; Original summary: " + ds.attrs["summary"]
+
+    ds.attrs["keywords"] = "oceanography, sediment transport"
+
+    return ds
+
+
+def convert(f, doi, title, summary):
+    try:
+        ds = xr.load_dataset(f, decode_times=False)
+        ds = stglib.utils.epic_to_cf_time(ds)
+    except:
+        # deal with 2D time (burst) data sets
+        with netCDF4.Dataset(f, "r+") as nc:
+            time = nc["time"][:]
+        # xarray won't open a dataset with vars and coords of the same name
+        ds = xr.open_dataset(f, decode_times=False, drop_variables="time")
+        # assign time to be the first of the burst
+        ds["time"] = xr.DataArray(
+            stglib.utils.epic_to_datetime(time[:, 0], ds.time2[:, 0].values),
+            dims="time",
+        )
+
+    dvorig = ds.data_vars
+
+    # round time to the nearest second to avoid weird netCDF datetime64 error
+    ds["time"] = ds["time"].dt.round(freq="S")
+    ds = add_title_history(ds, doi, title, summary)
+
+    ds = ds.squeeze()
+    if "depth" in ds:  # depth not in waves files usually
+        ds = ds.rename({"depth": "z"})
+        ds["z"].attrs["positive"] = "down"
+        ds["z"].attrs["long_name"] = "depth of sensor below mean water level"
+
+    ds.attrs["Conventions"] = "CF-1.6, ACDD-1.3"
+
+    if "lon" in ds and "lat" in ds and "longitude" not in ds and "latitude" not in ds:
+        ds = ds.rename({"lon": "longitude", "lat": "latitude"})
+    else:
+        ds["longitude"] = xr.DataArray([ds.attrs["longitude"]], dims="longitude")
+        ds["latitude"] = xr.DataArray([ds.attrs["latitude"]], dims="latitude")
+
+    for d in ds.coords:
+        ds[d].encoding["_FillValue"] = None
+    # no _FillValue for lat, lon just to be safe
+    for d in ["latitude", "longitude"]:
+        ds[d].encoding["_FillValue"] = None
+
+    # CF: Add axis attr
+    ds["time"].attrs["axis"] = "T"
+    if "z" in ds:
+        ds["z"].attrs["axis"] = "Z"
+        ds["z"].attrs["units"] = "m"
+    ds["longitude"].attrs["axis"] = "X"
+    ds["latitude"].attrs["axis"] = "Y"
+
+    ds = portal.assign_standard_names(ds)
+
+    ds["feature_type_instance"] = os.path.split(f)[1].split(".")[0]
+    ds["feature_type_instance"].attrs["cf_role"] = "timeseries_id"
+
+    for k in ds.data_vars:
+        ds[k].attrs["coverage_content_type"] = "physicalMeasurement"
+
+    # ADD STUFF FOR PORTAL COMPATIBILITY
+    # see https://github.com/USGS-CMG/usgs-cmg-portal/issues/289
+    ds.attrs["experiment_id"] = os.path.split(f)[1][0:5]
+    ds.attrs["metadata_link"] = "https://doi.org/10.5066/" + doi
+    # already have MOORING
+    ds.attrs["id"] = os.path.split(f)[1].split(".")[0]
+    ds.attrs["datasetID"] = os.path.split(f)[1].split(".")[0]
+    ds.attrs["project"] = "CMG_Portal"
+
+    # ERDDAP
+    ds.attrs["cdm_timeseries_variables"] = "feature_type_instance, latitude, longitude"
+
+    def remove_problematic_attrs(ds):
+        for variable in ds.variables.values():
+            if "coordinates" in variable.attrs:
+                del variable.attrs["coordinates"]
+
+    remove_problematic_attrs(ds)
+
+    for d in ds.data_vars:
+        if d not in [
+            "water_depth",
+            "feature_type_instance",
+            "z",
+            "latitude",
+            "longitude",
+        ]:
+            if "z" in ds[d].coords:
+                ds[d].attrs["coordinates"] = "time z latitude longitude"
+            else:
+                ds[d].attrs["coordinates"] = "time latitude longitude"
+
+    # CF Compliance Stuff
+    ds = portal.ensure_attr_dtype(ds)
+    # ACDD stuff
+    ds = portal.acdd_attrs(ds)
+    # Ensure valid_range is valid
+    ds = portal.ensure_valid_range(ds)
+    if "CREATION_DATE" in ds.attrs:
+        ds.attrs["date_created"] = pd.Timestamp(ds.attrs["CREATION_DATE"]).isoformat()
+    else:
+        ds.attrs["date_created"] = pd.Timestamp.now(tz="utc").isoformat()
+    ds.attrs["time_coverage_start"] = pd.Timestamp(ds["time"][0].values).isoformat()
+    ds.attrs["time_coverage_end"] = pd.Timestamp(ds["time"][-1].values).isoformat()
+    ds.attrs["time_coverage_duration"] = (
+        pd.Timestamp(ds.attrs["time_coverage_end"])
+        - pd.Timestamp(ds.attrs["time_coverage_start"])
+    ).isoformat()
+    ds.attrs["time_coverage_resolution"] = pd.Timedelta(
+        ds.time.diff(dim="time").median().values
+    ).isoformat()
+    ds.attrs["standard_name_vocabulary"] = "CF Standard Name Table v66"
+    ds.attrs["naming_authority"] = "gov.usgs.cmgp"
+    ds.attrs["institution"] = "USGS Coastal and Marine Geology Program"
+
+    ds["time"].attrs["long_name"] = "time of measurement"
+    ds["time"].encoding["dtype"] = "i4"
+    ds["time"].attrs["standard_name"] = "time"
+
+    if len(ds.dims) == 1:
+        ds.attrs["featureType"] = "timeSeries"
+    elif len(ds.dims) == 2:
+        ds.attrs["featureType"] = "timeSeriesProfile"
+
+    # check for 0-length attributes (this causes problems with ERDDAP) and set to an empty string
+    for k in ds.attrs:
+        if isinstance(ds.attrs[k], np.ndarray) and not ds.attrs[k].size:
+            ds.attrs[k] = ""
+
+    dvfinal = ds.data_vars
+    Path(os.path.split(f)[0] + "/clean").mkdir(parents=True, exist_ok=True)
+    ds.to_netcdf(os.path.split(f)[0] + "/clean/" + os.path.split(f)[1])
+
+    return os.path.split(f)[1], "difference in data_vars:", (set(dvorig) ^ set(dvfinal))
--- portal.py
+++ portal.py
@@ -1,61 +1,168 @@
+import numpy as np
+import subprocess
+
+
+def check_compliance_system(f, test):
+    return subprocess.run(["compliance-checker", "-t", test, f], capture_output=True)
+
+
 def acdd_attrs(ds):
     # stuff for ACDD
-    ds.attrs['creator_url'] = 'https://www.usgs.gov/'
-    ds.attrs['publisher_url'] = 'https://www.usgs.gov/'
-    ds.attrs['source'] = 'USGS'
-    ds.attrs['geospatial_lat_min'] = ds['latitude'].min().values
-    ds.attrs['geospatial_lat_max'] = ds['latitude'].max().values
-    ds.attrs['geospatial_lon_min'] = ds['longitude'].min().values
-    ds.attrs['geospatial_lon_max'] = ds['longitude'].max().values
+    ds.attrs["creator_url"] = "https://www.usgs.gov/"
+    ds.attrs["publisher_url"] = "https://www.usgs.gov/"
+    ds.attrs["source"] = "USGS"
+    ds.attrs["geospatial_lat_min"] = ds["latitude"].min().values
+    ds.attrs["geospatial_lat_max"] = ds["latitude"].max().values
+    ds.attrs["geospatial_lon_min"] = ds["longitude"].min().values
+    ds.attrs["geospatial_lon_max"] = ds["longitude"].max().values
 
     return ds
 
-standard_names = {'P_1': 'sea_water_pressure',
-                  'Tz': 'sea_surface_wave_mean_period',
-                  'wp_peak': 'sea_surface_wave_period_at_variance_spectral_density_maximum',
-                  'Hmean': 'sea_surface_wave_mean_height',
-                  'H10': 'sea_surface_wave_mean_height_of_highest_tenth',
-                  'u_1205': 'eastward_sea_water_velocity',
-                  'v_1206': 'northward_sea_water_velocity',
-                  'w_1204': 'upward_sea_water_velocity',
-                  'P_4023': 'sea_water_pressure_due_to_sea_water',
-                  'T_28': 'sea_water_temperature',
-                  'C_51': 'sea_water_electrical_conductivity',
-                  'S_41': 'sea_water_salinity',
-                  'Trb1_980': 'sea_water_turbidity',
-                  'Trb_980': 'sea_water_turbidity',
-                  'SED_981': 'mass_concentration_of_suspended_matter_in_sea_water',
-                  'Ptch_1216': 'platform_pitch_angle',
-                  'Roll_1217': 'platform_roll_angle',
-                  'Hdg_1215': 'platform_orientation',
-                  'SDP_850': 'sea_water_pressure',
-                  'z': 'height'}
 
-long_names = {'burst': 'Burst number'}
+standard_names = {
+    "P_1": "sea_water_pressure",
+    "P_1294": "sea_water_pressure",
+    "SDP_850": "sea_water_pressure",
+    "P_4023": "sea_water_pressure_due_to_sea_water",
+    "u_1205": "eastward_sea_water_velocity",
+    "v_1206": "northward_sea_water_velocity",
+    "w_1204": "upward_sea_water_velocity",
+    "u_1205min": "eastward_sea_water_velocity",
+    "v_1206min": "northward_sea_water_velocity",
+    "w_1204min": "upward_sea_water_velocity",
+    "u_1205max": "eastward_sea_water_velocity",
+    "v_1206max": "northward_sea_water_velocity",
+    "w_1204max": "upward_sea_water_velocity",
+    "USTD_4097": "eastward_sea_water_velocity",
+    "VSTD_4098": "northward_sea_water_velocity",
+    "WSTD_4099": "upward_sea_water_velocity",
+    "T_28": "sea_water_temperature",
+    "Tx_1211": "sea_water_temperature",
+    "C_50": "sea_water_electrical_conductivity",
+    "C_51": "sea_water_electrical_conductivity",
+    "S_41": "sea_water_salinity",
+    "ST_70": "sea_water_sigma_t",
+    "SV_80": "speed_of_sound_in_sea_water",
+    "Trb2_980": "sea_water_turbidity",
+    "Trb1_980": "sea_water_turbidity",
+    "Trb_980": "sea_water_turbidity",
+    "SED_981": "mass_concentration_of_suspended_matter_in_sea_water",
+    "SED1_981": "mass_concentration_of_suspended_matter_in_sea_water",
+    "Sed_981": "mass_concentration_of_suspended_matter_in_sea_water",
+    "Ptch_1216": "platform_pitch_angle",
+    "Roll_1217": "platform_roll_angle",
+    "Hdg_1215": "platform_orientation",
+    "wh_4061": "sea_surface_wave_significant_height",
+    "mwh_4064": "sea_surface_wave_maximum_height",
+    "wp_4060": "sea_surface_wave_mean_period",
+    "hght_18": "sea_surface_height",
+    "Tz": "sea_surface_wave_mean_period",
+    "wp_peak": "sea_surface_wave_period_at_variance_spectral_density_maximum",
+    "Hmean": "sea_surface_wave_mean_height",
+    "H10": "sea_surface_wave_mean_height_of_highest_tenth",
+    "wvdir": "sea_surface_wave_from_direction",
+    "z": "height",
+}
 
-cell_methods = {'SDP_850': 'time: standard_deviation'}
+# long_names = {'burst': 'Burst number',
+#               'Werr_1201': 'ADCP error velocity (difference between the two estimates of vertical velocity)',
+#               'PGd_1203': 'Percent good pings'}
+
+cell_methods = {
+    "SDP_850": "time: standard_deviation",
+    "u_1205min": "time: minimum",
+    "v_1206min": "time: minimum",
+    "w_1204min": "time: minimum",
+    "u_1205max": "time: maximum",
+    "v_1206max": "time: maximum",
+    "w_1204max": "time: maximum",
+    "USTD_4097": "time: standard_deviation",
+    "VSTD_4098": "time: standard_deviation",
+    "WSTD_4099": "time: standard_deviation",
+}
+
 
 def assign_standard_names(ds):
     # drop all vars without a standard_name
     for d in ds.data_vars:
-        if (d not in standard_names) and (d not in long_names):
+        if (d not in standard_names) and ("long_name" not in ds[d].attrs):
             ds = ds.drop(d)
+
+    # drop time2
+    if "time2" in ds:
+        ds = ds.drop("time2")
 
     for k in standard_names:
         if k in ds:
-            if 'standard_name' not in ds[k].attrs:
-                ds[k].attrs['standard_name'] = standard_names[k]
+            if "standard_name" not in ds[k].attrs:
+                ds[k].attrs["standard_name"] = standard_names[k]
     for k in cell_methods:
         if k in ds:
-            if 'cell_methods' not in ds[k].attrs:
-                ds[k].attrs['cell_methods'] = cell_methods[k]
+            if "cell_methods" not in ds[k].attrs:
+                ds[k].attrs["cell_methods"] = cell_methods[k]
 
-    if 'T_28' in ds:
-        if ds['T_28'].attrs['units'] == 'C':
-            ds['T_28'].attrs['units'] = 'degree_C'
+    if "T_28" in ds:
+        if ds["T_28"].attrs["units"] == "C":
+            ds["T_28"].attrs["units"] = "degree_C"
 
-    if 'burst' in ds:
-        if ds['burst'].attrs['units'] == 'none':
-            ds['burst'].attrs['units'] = '1'
+    if "Tx_1211" in ds:
+        if (ds["Tx_1211"].attrs["units"] == "C") or (
+            ds["Tx_1211"].attrs["units"] == "degrees.C"
+        ):
+            ds["Tx_1211"].attrs["units"] = "degree_C"
+
+    if "latitude" in ds:
+        ds["latitude"].attrs["standard_name"] = "latitude"
+        ds["latitude"].attrs["long_name"] = "sensor latitude"
+        ds["latitude"].attrs["units"] = "degrees_north"
+
+    if "longitude" in ds:
+        ds["longitude"].attrs["standard_name"] = "longitude"
+        ds["longitude"].attrs["long_name"] = "sensor longitude"
+        ds["longitude"].attrs["units"] = "degrees_east"
+
+    if "burst" in ds:
+        if "units" in ds["burst"].attrs:
+            if ds["burst"].attrs["units"] == "none":
+                ds["burst"].attrs["units"] = "1"
+        else:
+            ds["burst"].attrs["units"] = "1"
+
+    if "P_1" in ds:
+        if ds["P_1"].attrs["units"] == "DB":
+            ds["P_1"].attrs["units"] = "dbar"
+    
+    if "P_1294" in ds:
+        if ds['P_1294'].attrs['units'].lower() == 'deca-pascals':
+            ds['P_1294'].attrs['units'] = 'dekapascals'
+
+    # rename attributes with a '.' in them (causes CF warning)
+    for k in list(
+        ds.attrs
+    ):  # needs to be list; can't have keys changing during iteration
+        if "." in k:
+            ds.attrs[k.replace(".", "_")] = ds.attrs.pop(k)
+
+    return ds
+
+
+def ensure_attr_dtype(ds):
+    for k in ds:
+        for a in ds[k].attrs:
+            if isinstance(ds[k].attrs[a], np.ndarray) or isinstance(ds[k].attrs[a], np.float64):
+                if ds[k].attrs[a].dtype != ds[k].dtype:
+                    # this typically happens on valid_range being a different type than its variable
+                    # so promote dtype of valid_range attr to be the same as its var
+                    ds[k].attrs[a] = ds[k].attrs[a].astype(ds[k].dtype)
+
+    return ds
+
+
+def ensure_valid_range(ds):
+    # mostly happens when valid_range is a space character
+    # remove it if so
+    for k in ds:
+        if ("valid_range" in ds[k].attrs) and (ds[k].attrs["valid_range"] == " "):
+            ds[k].attrs.pop("valid_range")
 
     return ds
